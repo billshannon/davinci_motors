2080abc446a28944ee386f0cccd2b99162a4b1ec
Bill Shannon <bill@billshannon.com>
2014-11-28 10:13:29 -0700

    add student model

---
+++ spec/factories/students.rb
@@ -0,0 +1,14 @@
+FactoryGirl.define do
+  factory :student do
+    date_of_birth "2014-11-26"
+eye_color "MyString"
+first_name "MyString"
+middle_name "MyString"
+last_name "MyString"
+hair_color "MyString"
+height "MyString"
+sex "MyString"
+weight "MyString"
+  end
+
+end
